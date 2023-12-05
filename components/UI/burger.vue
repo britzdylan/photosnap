4dfb1ee68f9488f5025a505fdf9a9a3de8f408a5
Dylan Britz <britzdylan@gmail.com>
2023-12-05 18:57:58 +0100

    Add burger component

---
+++ components/UI/burger.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="burger" @click="toggle">
+    <div
+      :class="{ 'rotate-45 !translate-y-0': isActive }"
+      class="burger__line -translate-y-1"></div>
+    <div
+      :class="{ '-rotate-45 !translate-y-0': isActive }"
+      class="burger__line translate-y-1"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const emit = defineEmits(['open', 'closed']);
+  const isActive = ref(false);
+
+  const toggle = () => {
+    isActive.value = !isActive.value;
+    isActive.value ? emit('open') : emit('closed');
+  };
+</script>
+
+<style scoped>
+  .burger {
+    @apply cursor-pointer flex flex-col gap-1 justify-center items-center w-8 h-8 relative z-10;
+  }
+
+  .burger__line {
+    @apply absolute w-full h-0.5  bg-pureBlack rounded-full origin-center transition-all duration-300 ease-in-out;
+  }
+</style>
