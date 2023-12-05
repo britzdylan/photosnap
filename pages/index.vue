93b5d5c6efd6af3cfaf1d62b06dda3d95a803d95
Dylan Britz <britzdylan@gmail.com>
2023-12-05 23:52:10 +0100

    Refactor home page template and add isZeroOrEven function

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <h1>Home page</h1>
-  </div>
+  <ContentBlock
+    v-for="(item, index) in home.blocks"
+    v-bind="{
+      ...item,
+      order: isZeroOrEven(index) ? 'normal' : 'reverse',
+      hero: index === 0,
+    }" />
 </template>
+
+<script setup lang="ts">
+  const home = useHome();
+  const isZeroOrEven = (num: number): boolean => {
+    console.log(num === 0 || num % 2 === 0);
+    return num === 0 || num % 2 === 0;
+  };
+</script>
+/
