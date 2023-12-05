fe810683a553b1827ac1976676f081f63243e3ad
Dylan Britz <britzdylan@gmail.com>
2023-12-05 21:27:35 +0100

    Add link component with props and slot

---
+++ components/UI/link.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLink @click="$emit('clicked')" :to="to" class="btn link dark">
+    <slot></slot>
+    <SvgoArrow />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    to: string;
+  }
+
+  defineProps<Props>();
+</script>
