099e314819a92c5f11bcaff12d088aed7b783374
Dylan Britz <britzdylan@gmail.com>
2023-12-05 19:27:01 +0100

    Add button component

---
+++ components/UI/button.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLink @click="$emit('clicked')" :to="to" class="btn primary">
+    <slot></slot>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    to: string;
+  }
+
+  defineProps<Props>();
+</script>
