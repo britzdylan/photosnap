8a55062ea1aab7c657447cd5caa3acd8693a1753
Dylan Britz <britzdylan@gmail.com>
2023-12-05 19:27:08 +0100

    Add navLinks component

---
+++ components/navLinks.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul class="hidden md:flex justify-center items-center">
+    <li v-for="item in links">
+      <NuxtLink :key="item.text" :to="item.href" class="nav-link">
+        {{ item.text }}
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import type 
+</script>
