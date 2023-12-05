9e4dc7d44762d2dc384a9a1e7ad33432d1fcef11
Dylan Britz <britzdylan@gmail.com>
2023-12-05 23:51:57 +0100

    Update main container margin in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex flex-col">
     <Navigation />
-    <main class="flex flex-col flex-grow-1 gap-0">
+    <main class="flex flex-col flex-grow-1 gap-0 mt-[72px] md:mt-0">
       <slot />
     </main>
     <Footer />
