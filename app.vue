eef6354f20429993559860f7468a190b4d154d41
Dylan Britz <britzdylan@gmail.com>
2023-12-05 14:09:14 +0100

    Update headers and paragraph in app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="max-w-screen-xl flex flex-col gap-4 mx-auto py-48">
+    <h1>Header 1</h1>
+    <h2>Header 2</h2>
+    <h3>Header 3</h3>
+    <h4>Header 4</h4>
+    <h5>Header 5</h5>
+    <h6>Header 6</h6>
+    <p>Paragraph</p>
   </div>
 </template>
