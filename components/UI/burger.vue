812bf523a03211020cf023fe68db85dc456b22f8
Dylan Britz <britzdylan@gmail.com>
2023-12-05 19:26:53 +0100

    Refactor burger component to use "open" prop instead of "isActive"

--- components/UI/burger.vue
+++ components/UI/burger.vue
@@ -1,27 +1,22 @@
 <template>
-  <div class="burger" @click="toggle">
+  <div class="burger">
     <div
-      :class="{ 'rotate-45 !translate-y-0': isActive }"
+      :class="{ 'rotate-45 !translate-y-0': open }"
       class="burger__line -translate-y-1"></div>
     <div
-      :class="{ '-rotate-45 !translate-y-0': isActive }"
+      :class="{ '-rotate-45 !translate-y-0': open }"
       class="burger__line translate-y-1"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-  const emit = defineEmits(['open', 'closed']);
-  const isActive = ref(false);
-
-  const toggle = () => {
-    isActive.value = !isActive.value;
-    isActive.value ? emit('open') : emit('closed');
-  };
+  defineProps<{ open: boolean }>();
+  const emit = defineEmits(['clicked']);
 </script>
 
 <style scoped>
   .burger {
-    @apply cursor-pointer flex flex-col gap-1 justify-center items-center w-8 h-8 relative z-10;
+    @apply cursor-pointer flex flex-col gap-1 justify-center items-center w-8 h-8 relative z-10 md:hidden;
   }
 
   .burger__line {
