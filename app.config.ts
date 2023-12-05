ce8fc312f30e82f31d75416ab99cdd46e2d40a88
Dylan Britz <britzdylan@gmail.com>
2023-12-05 16:43:22 +0100

    Update app.config.ts with footer links and social media icons

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   title: 'Photosnap',
   navigation: {
-    header: {
+    shared: {
       logo: {
         src: '/images/shared/logo.svg',
         alt: 'Photosnap logo',
@@ -16,6 +16,118 @@ export default defineAppConfig({
         text: 'Get an invite',
         href: '/invite',
       },
+    },
+    footer: {
+      links: [{ text: 'Home', href: '/' }],
+      social: [
+        {
+          icon: 'facebook',
+          href: 'https://www.facebook.com',
+        },
+        {
+          icon: 'youtube',
+          href: 'https://www.youtube.com',
+        },
+        {
+          icon: 'twitter',
+          href: 'https://www.twitter.com',
+        },
+        {
+          icon: 'pinterest',
+          href: 'https://www.pinterest.com',
+        },
+        {
+          icon: 'instagram',
+          href: 'https://www.instagram.com',
+        },
+      ],
+    },
+  },
+  layout: {
+    callToAction: {
+      title: 'We’re in beta. Get your invite today!',
+      cta: {
+        text: 'Get an invite',
+        href: '/invite',
+      },
+      image: 'rocks.png',
+    },
+  },
+  pages: {
+    home: {
+      blocks: [
+        {
+          title: 'Create and share your photo stories.',
+          subtitle:
+            'Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.',
+          cta: {
+            text: 'Get an invite',
+            href: '/invite',
+          },
+          image: '',
+        },
+        {
+          title: 'BEAUTIFUL STORIES EVERY TIME',
+          subtitle:
+            'We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.',
+          cta: {
+            text: 'Veiw the stories',
+            href: '/stories',
+          },
+          image: '',
+        },
+        {
+          title: 'DESIGNED FOR EVERYONE',
+          subtitle:
+            'Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it.',
+          cta: {
+            text: 'Veiw the stories',
+            href: '/stories',
+          },
+          image: '',
+        },
+      ],
+    },
+    features: {
+      blocks: [
+        {
+          title: 'Features',
+          subtitle:
+            'We make sure all of our features are designed to be loved by every aspiring and even professional photograpers who wanted to share their stories.',
+          image: 'camera_leg.png',
+        },
+      ],
+    },
+    pricing: {
+      blocks: [
+        {
+          title: 'Pricing',
+          subtitle:
+            'Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos.',
+          image: 'silouete.png',
+        },
+      ],
+      plans: [
+        {
+          title: 'Basic',
+          description:
+            'Includes basic usage of our platform. Recommended for new and aspiring photographers.',
+          price: {
+            monthly: 19,
+            annually: 190,
+          }, 
+          features: [
+            'Unlimited Story Posting',
+            'Unlimited Photo Upload',
+            'Embedding Custom Content',
+            'Customize Metadata',
+            'Advanced Metrics',
+            'Photo Downloads',
+            'Search Engine Indexing',
+            'custom Analytics',
+          ],
+        },
+      ],
     },
   },
 });
