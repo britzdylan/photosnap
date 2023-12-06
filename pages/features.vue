3a485922853bf4cdedc4d037521c42fa46ed8609
Dylan Britz <britzdylan@gmail.com>
2023-12-06 10:00:29 +0100

    Refactor features.vue component

--- pages/features.vue
+++ pages/features.vue
@@ -1,6 +1,21 @@
 <template>
-    <div>
-      <h1>Home page</h1>
-    </div>
-  </template>
-  
+  <ContentBlock
+    v-for="(item, index) in page.blocks"
+    v-bind="{
+      ...item,
+      order: getOrder(index),
+      hero: index === 0,
+    }" />
+</template>
+
+<script setup lang="ts">
+  const page = useFeatures();
+
+  const isZeroOrEven = (num: number): boolean => {
+    return num === 0 || num % 2 === 0;
+  };
+
+  const getOrder = (index: number): 'normal' | 'reverse' => {
+    return isZeroOrEven(index) ? 'normal' : 'reverse';
+  };
+</script>
