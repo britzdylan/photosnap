6e978671152a35829c7429bf909d3b3060234d49
Dylan Britz <britzdylan@gmail.com>
2023-12-06 13:13:01 +0100

    Add features section to features.vue

--- pages/features.vue
+++ pages/features.vue
@@ -6,9 +6,17 @@
       order: getOrder(index),
       hero: index === 0,
     }" />
+  <section class="w-full ppx-8 py-24">
+    <div class="features-wrapper">
+      <template v-for="(item, index) in features">
+        <UIFeature v-bind="item" />
+      </template>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
+  import features from '@/data/features.json';
   const page = useFeatures();
 
   const isZeroOrEven = (num: number): boolean => {
