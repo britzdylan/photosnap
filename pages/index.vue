f975e1d63c53a2f857e63da3c8bf203d7030feab
Dylan Britz <britzdylan@gmail.com>
2023-12-06 13:09:21 +0100

    Add features section to home page

--- pages/index.vue
+++ pages/index.vue
@@ -11,10 +11,18 @@
       <StoriesCard v-bind="item" v-if="index < 5 && !item.featured" />
     </template>
   </section>
+  <section class="w-full ppx-8 py-24">
+    <div class="features-wrapper">
+      <template v-for="(item, index) in features">
+        <UIFeature v-bind="item" v-if="index < 3" />
+      </template>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
   import stories from '@/data/stories.json';
+  import features from '@/data/features.json';
   const home = useHome();
 
   const isZeroOrEven = (num: number): boolean => {
