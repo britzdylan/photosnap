a939f18450005c663264eb6879936de3bf87f01d
Dylan Britz <britzdylan@gmail.com>
2023-12-06 12:35:24 +0100

    Refactor stories.vue component to display featured and non-featured stories

--- pages/stories.vue
+++ pages/stories.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <h1>Home page</h1>
-  </div>
+  <section class="w-full min-h-650px max-w-[1920px] mx-auto">
+    <template v-for="item in stories.stories">
+      <StoriesFeatured v-bind="item" v-if="item.featured" />
+    </template>
+  </section>
+  <section class="stories-wrapper">
+    <template v-for="item in stories.stories">
+      <StoriesCard v-bind="item" v-if="!item.featured" />
+    </template>
+  </section>
 </template>
+
+<script setup lang="ts">
+  import stories from '@/data/stories.json';
+</script>
