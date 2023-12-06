dedcde99b6b1d24bfa46a4f913a1f21fac824833
Dylan Britz <britzdylan@gmail.com>
2023-12-06 11:09:21 +0100

    Refactor layout component to show CallToAction based on current route

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,12 +4,25 @@
     <main class="flex flex-col flex-grow-1 gap-0 mt-[72px] md:mt-0">
       <slot />
     </main>
-    <CallToAction v-bind="callToAction" />
+    <CallToAction v-show="showCTA" v-bind="layout.callToAction" />
     <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
   const layout = useLayout();
-  const { callToAction } = layout.value;
+  const router = useRouter();
+  const route = useRoute();
+  const currentRoute = ref(route.name);
+
+  router.beforeEach((to, from, next) => {
+    console.log('to', to.name);
+    currentRoute.value = to.name;
+    next();
+  });
+
+  const showCTA = computed((): boolean => {
+    // @ts-ignore
+    return ['pricing', 'features'].includes(currentRoute.value!);
+  });
 </script>
