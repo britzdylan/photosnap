86dbdd6c7514591812397ff2a191242b4b4097aa
Dylan Britz <britzdylan@gmail.com>
2023-12-06 12:20:37 +0100

    Add stories section to index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,20 @@
       order: getOrder(index),
       hero: index === 0,
     }" />
+  <section
+    class="stories-wrapper">
+    <template v-for="(item, index) in stories.stories">
+      <StoriesCard v-bind="item" v-if="index < 5 && !item.featured" />
+    </template>
+  </section>
 </template>
 
 <script setup lang="ts">
+  import type { Story } from '@/components/Stories/card.vue';
+  import stories from '@/data/stories.json';
   const home = useHome();
 
+  console.log('stories', stories);
   const isZeroOrEven = (num: number): boolean => {
     return num === 0 || num % 2 === 0;
   };
